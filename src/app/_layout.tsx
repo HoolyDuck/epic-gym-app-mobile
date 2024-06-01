2bf856d26083a7dc8e7016682d6ed68b0614145b
HoolyDuck <nazar260804@gmail.com>
2024-06-01 18:20:17 +0300

    * fix fonts not loading if not visiting index

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,46 @@
+import { Colors } from "@/constants";
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import { useCallback } from "react";
+import { View, StyleSheet } from "react-native";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    Montserrat: require("@/assets/fonts/Montserrat.ttf"),
+    MontserratBold: require("@/assets/fonts/MontserratBold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (loaded || error) {
+      await SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    />
+    <View
+      onLayout={onLayoutRootView}
+      style={styles.container}
+    >
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.BG,
+    justifyContent: "center",
+    width: "100%",
+  },
+});
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,29 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Link, SplashScreen } from "expo-router";
 import { Colors } from "@/constants";
-import { useCallback } from "react";
-import { useFonts } from "expo-font";
 
 const Index = () => {
-  const [loaded, error] = useFonts({
-    Montserrat: require("@/assets/fonts/Montserrat.ttf"),
-    MontserratBold: require("@/assets/fonts/MontserratBold.ttf"),
-  });
 
-  const onLayoutRootView = useCallback(async () => {
-    if (loaded || error) {
-      await SplashScreen.hideAsync();
-    }
-  }, [loaded, error]);
-
-  if (!loaded) {
-    return null;
-  }
 
   return (
     <View
       style={styles.container}
-      onLayout={onLayoutRootView}
     >
       <Text>WellCum</Text>
       <Link href="/login">Login</Link>
