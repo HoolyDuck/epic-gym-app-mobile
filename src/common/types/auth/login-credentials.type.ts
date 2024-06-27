859c9fbb08b4f9409a0d58adb5af29a326aaec53
HoolyDuck <nazar260804@gmail.com>
2024-06-27 16:12:17 +0300

    * username to email in login cred type

--- src/common/types/auth/login-credentials.type.ts
+++ src/common/types/auth/login-credentials.type.ts
@@ -1,5 +1,5 @@
 type LoginCredentials = {
-    username: string;
+    email: string;
     password: string;
 };
 
--- src/lib/hooks/use-login.hook.ts
+++ src/lib/hooks/use-login.hook.ts
@@ -2,21 +2,13 @@ import { useLoginMutation } from "@/lib/apis/auth.api";
 import { asyncStorageHelper } from "@/common/helpers";
 import { AsyncStorageKeys } from "@/common/enums";
 import { router } from "expo-router";
-
-type LoginFormData = {
-  email: string;
-  password: string;
-};
+import { LoginCredentials } from "@/common/types";
 
 const useLogin = () => {
   const [login] = useLoginMutation();
 
-  const handleLogin = async (credentials: LoginFormData) => {
-    const { data, error } = await login({
-      username: credentials.email,
-      password: credentials.password,
-    });
-
+  const handleLogin = async (credentials: LoginCredentials) => {
+    const { data, error } = await login(credentials);
     if (error) {
       console.log("Error logging in", error);
       return;
